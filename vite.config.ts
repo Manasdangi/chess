9eab4c3c1fe6c097194a9337746a32fe2766737d
Manasdangi <manasraj9669@gmail.com>
2025-04-27 10:42:32 +0530

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"', // This explicitly sets NODE_ENV to production
+  },
 });
